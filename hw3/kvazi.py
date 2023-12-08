584a3df960b41b257875c4ce3618bc94392f7625
Galina Savon <g.savon11@yanex.ru>
2023-12-08 16:41:18 +0300

    chto-to ne tak

--- hw3/kvazi.py
+++ hw3/kvazi.py
@@ -28,9 +28,22 @@ def show_graph(x_a, y_a, z_a, z_p, h, tau):
     return
 
 
+def show_s_res(x, ys, step, u_a):
+    print("YSSS", ys)
+    for i in range(len(ys)):
+        plt.plot(x, ys[i], label=f's_{i}')
+        plt.xlabel('x')
+    plt.plot(x, u_a, label=f'u_a')
+    plt.ylabel('S_i')
+    plt.legend()
+    plt.title(f'Слои аналитического решения S для n = {step}')
+    plt.show()
+    return
+
+
 def analytics(x, t):
     X, T = np.meshgrid(x, t)
-    u = u_analytics(X, T)
+    u = u_analytics(T, X)
     return X, T, u
 
 
@@ -40,30 +53,34 @@ def count_s(x_size, tau, h, prev_layer, s_prev, first_elem, last_elem):
     for l in range(1, x_size - 1):
         k1[l] = (s_prev[l+1] ** NU + s_prev[l] ** NU) / 2 * h ** 2
         k2[l] = (s_prev[l] ** NU + s_prev[l-1] ** NU) / 2 * h ** 2
-    a = -k2
-    b = 1/tau + k1 + k2
-    c = -1 * k1
-    d = prev_layer/tau
+    c = k1
+    b = -1/tau - k1 - k2
+    a = k2
+    d = -prev_layer/tau
+    print("K1, K2", k1, k2)
+    print("ABCD", a, b, c, d)
 
     alpha = np.zeros((x_size))
     beta = np.zeros((x_size))
-    alpha[0] = -(a[0] / b[0])
-    beta[0] = d[0] / b[0]
-    for i in range(x_size):
+    alpha[0] = 0
+    beta[0] = first_elem
+    alpha[-1] = 0
+    beta[-1] = last_elem
+    for i in range(1, x_size - 1):
         alpha[i] = -a[i] / (b[i] + c[i] * alpha[i - 1])
         beta[i] = (d[i] - c[i] * beta[i - 1]) / (b[i] + c[i] * alpha[i - 1])
 
     s = np.zeros((x_size))
     # print(s.size)
     s[-1] = last_elem
-    s[0] = first_elem
-    for i in range(x_size - 2, 0, -1):
+    #s[0] = first_elem
+    for i in range(x_size - 2, -1, -1):
         s[i] = alpha[i] * s[i + 1] + beta[i]
-
+        print("i, s:", i, s[i])
     return s
 
 
-def progonka(x, t, h, tau):
+def progonka(x, t, h, tau, u_a):
     t_size = t.size
     x_size = x.size
     u = np.zeros((x_size, t_size))
@@ -76,33 +93,46 @@ def progonka(x, t, h, tau):
         u[0, n] = C1 ** (2 / NU) * (C0 - 2 * (2 + NU) * t[n] / NU) ** (-1/NU)
         u[-1, n] = (1 + C1) ** (2 / NU) * (C0 - 2 * (2 + NU) * t[n] / NU) ** (-1/NU)
     # print(u)
+    print("T, XXXXX:", t_size, x_size)
+    print(u)
 
     u = u.transpose()
+
+    print("T, XXXXX:", t_size, x_size)
+    print(u)
     for now_layer in range(1, t_size):
         s_res = u[now_layer - 1] # в начале одно и то же
-        s_res = count_s(x_size, tau, h, u[now_layer - 1], s_res, u[now_layer][0], u[now_layer][-1])
-
-        s_diff_otn = np.zeros((x_size))
-        for i in range(s_diff_otn.size):
-            s_diff_otn[i] = 1
+        s_res_arr = []
+        s_res_arr.append(s_res)
+        s_res_new = count_s(x_size, tau, h, u[now_layer - 1], s_res, u[now_layer, 0], u[now_layer, -1])
+        s_res_arr.append(s_res_new)
+        s_diff = abs(s_res_new - s_res)
+        s_diff_otn = s_diff / abs(s_res)
+        # s_diff_otn = np.zeros((x_size))
+        # for i in range(s_diff_otn.size):
+        #     s_diff_otn[i] = 1
         ITER = 0
-        print(s_diff_otn, max(s_diff_otn))
+        # print(s_diff_otn, max(s_diff_otn))
+        s_res = s_res_new
 
         while max(s_diff_otn) > EPS:
             ITER += 1
-            s_res_new = count_s(x_size, tau, h, u[now_layer - 1], s_res, u[now_layer][0], u[now_layer][-1])
+            s_res_new = count_s(x_size, tau, h, u[now_layer - 1], s_res, u[now_layer, 0], u[now_layer, -1])
             s_diff = abs(s_res_new - s_res)
             s_diff_otn = s_diff / s_res
             s_res = s_res_new
-
-            print(s_diff_otn, max(s_diff_otn))
+            s_res_arr.append(s_res)
+            #print(s_diff_otn, max(s_diff_otn))
         u[now_layer] = s_res
+        show_s_res(x, s_res_arr, now_layer, u_a[now_layer])
+        print(s_diff_otn, max(s_diff_otn))
+        print("s_res", s_res)
         print('now_la = ', now_layer, 'inters = ', ITER)
-
+    print(u)
     return u
 
 
-h_kol = 5
+h_kol = 7
 t_kol = h_kol + 2
 h = 1/(h_kol - 1)
 tau = 1/(t_kol - 1)
@@ -111,10 +141,6 @@ x = np.linspace(0, 1, h_kol)
 t = np.linspace(0, 1, t_kol)
 X_a, T_a, u_a = analytics(x, t)
 
-print(u_a[-1])
-
-u_p = progonka(x, t, h, tau)
-
-print(u_p[-1])
+u_p = progonka(x, t, h, tau, u_a)
 
 show_graph(X_a, T_a, u_a, u_p, h, tau)
